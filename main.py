727e3fb724ea9acbbfdc86d32004b713410b67a1
Mário Lucas de Almeida Silva <mario.lucas250702@gmail.com>
2023-01-26 22:22:39 -0300

    chore: add principal functions for project.

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from management import product_handler
+
+
+def main():
+    product_handler.get_product_by_id(),
+    product_handler.get_products_by_type()
+
+
 if __name__ == "__main__":
+
+    main()
     # Seus prints de teste aqui
     ...
---
+++ management/product_handler.py
@@ -0,0 +1,27 @@
+from menu import products
+
+
+def get_product_by_id(id: int):
+    if id in products:
+        return []
+
+
+def get_products_by_type(type: str):
+    if type in products["id"]:
+        return []
+    else:
+        return []
+
+
+def add_products(type: str):
+    if type in products["id"]:
+        return []
+    else:
+        return []
+
+
+def calculate_tab(type: str):
+    if type in products["id"]:
+        return []
+    else:
+        return []
